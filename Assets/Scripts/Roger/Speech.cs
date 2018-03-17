d8bce60c60da31a47cc759cd01bc230823d22b98
Kadi <kadermat@students.zhaw.ch>
2018-03-17 21:05:52 +0100

    added speech for earth textures

---
+++ Assets/Scripts/Roger/Speech.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class Speech : MonoBehaviour
+{
+    ChangeMaterial changeMaterial;
+    TextureSwap changeTexture;
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    // Use this for initialization
+    void Start()
+    {
+        changeMaterial = GetComponent<ChangeMaterial>();
+        changeTexture = GetComponent<TextureSwap>();
+
+        keywords.Add("show", () =>
+        {
+            // Call the xxxx method on the earth object.
+            PreAgeCalled();
+            changeTexture.changeTexture();
+            //changeMaterial.changeMaterial();
+
+        });
+
+        keywords.Add("back", () =>
+        {
+            // Call the xxxx method on the earth object.
+            changedMaterialBack();
+            changeTexture.changeTextureBack();
+            //changeMaterial.changeMaterialBack();
+
+        });
+
+        // Tell the KeywordRecognizer about our keywords.
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        // Register a callback for the KeywordRecognizer and start recognizing!
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+
+    void PreAgeCalled()
+    {
+        print("PreAge Speech works");
+    }
+
+    void changedMaterialBack()
+    {
+        print("Material is back to normal");
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/Roger/TextureSwap.cs
@@ -0,0 +1,29 @@
+﻿// Change renderer's texture each changeInterval/
+// seconds from the texture array defined in the inspector.
+
+using UnityEngine;
+using System.Collections;
+
+public class TextureSwap : MonoBehaviour
+{
+    public Texture[] textures;
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    public void changeTexture()
+    {
+        rend.material.mainTexture = textures[1];
+    }
+
+    public void changeTextureBack()
+    {
+        rend.material.mainTexture = textures[0];
+    }
+    void Update()
+    {
+    }
+}

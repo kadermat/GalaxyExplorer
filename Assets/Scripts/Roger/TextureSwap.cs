2da09de9c8d70cb9d88638ae16717062c5a98285
Roger Fehr <fehrrog2@students.zhaw.ch>
2018-03-28 18:18:03 +0200

    -added Textures from Marble 10000 -added Earth Before XXX Years Text -changed Texture Swap Script with Text Display on EarthView and SetFloat -added DualMaps shader (try for smooth texture swap -added Snow Font for Display text earth before XXX Years

--- Assets/Scripts/Roger/TextureSwap.cs
+++ Assets/Scripts/Roger/TextureSwap.cs
@@ -3,13 +3,21 @@
 
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class TextureSwap : MonoBehaviour
 {
-	private float duration = 0.2f;
+	private float duration = 0.5f;
 	private bool changeTextureOK = false;
+	private int preAgeStartTime = 19000;
+	private int preAgeTime = 0;
+	private int preAgeCounter = 0;
+	private string BCAD = "BC";
 	public Texture[] textures;
-	Renderer rend;
+	public Text preAgeText;
+	
+
+	private Renderer rend;
 
 	void Start()
 	{
@@ -41,12 +49,34 @@ public class TextureSwap : MonoBehaviour
 		{
 			if (changeTextureOK)
 			{
+				if (BCAD == "BC")
+				{
+					preAgeTime = preAgeStartTime - preAgeCounter;
+				}
+
+				if (preAgeTime == 0 && BCAD == "BC")
+				{
+					BCAD = "AD";
+					preAgeCounter = 0;
+				}
+
+				if (BCAD == "AD")
+				{
+					preAgeTime = preAgeTime + preAgeCounter;
+				}
+								
+				preAgeText.text = "Earth: " + preAgeTime + " " + BCAD;
 				rend.material.mainTexture = textures[i];
-				print("Texture position" + i);
+				rend.material.SetFloat("_Blend", 2.0F);
+				print("Texture Name" + textures[i]);
 				yield return new WaitForSeconds(duration);
+				preAgeCounter = preAgeCounter + 500; 
 			}
 		}
 		changeTextureBack();
+		preAgeCounter = 0;
+		BCAD = "BC";
+		preAgeText.text = "";
 	}
 
 	//public IEnumerator Wait()
